9f047b1ca5b423dcab9bcd64d3c2aa40af54b4c6
vijipravasthi-pixel <vijipravasthi@gmail.com>
2026-07-19 22:02:39 +0530

    . java

---
+++ checkNumber.java
@@ -0,0 +1,20 @@
+public class checkNumber{
+	public static void main(String[] args) {
+	int num=-7;
+	System.out.println("Number is:"+ num);
+	if(num>0)
+	{
+	    System.out.println("This is a Positive numbet");  	
+	}
+	else if(num<0)
+	{
+	    System.out.println("This is a Negative number");
+	}
+	else
+	{
+	    System.out.println("This is Zero");
+	}
+	}
+}
+	
+
