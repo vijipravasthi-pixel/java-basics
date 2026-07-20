553f2e684be5da718351769f54fcbc26c77b2f98
vijipravasthi-pixel <vijipravasthi@gmail.com>
2026-07-20 20:08:13 +0530

    . java

---
+++ variable.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 25;
+        double height = 5.9;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "viji";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
